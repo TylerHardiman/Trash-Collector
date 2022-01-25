700c4e781d35d98251e65189aea0b03ea4507ff4
Tyler Hardiman <Fulldadmode89@gmail.com>
2022-01-25 16:07:12 -0500

    Created the Employee Model, Implemented index function for views.py employees, added a static folder and migrated the employee model to the database.

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=5)
+    weekly_pickup = models.CharField(max_length=9)
+    one_time_pickup = models.DateField(null=True, blank=True)
+    remaining_trash_pickup = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
 # TODO: Create an Employee model with properties required by the user stories
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+from .models import Employee
+
+
+
 
 # Create your views here.
 
@@ -9,5 +17,34 @@ from django.apps import apps
 
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+
+    Customer = apps.get_model('customer.Customer')
+
+    logged_in_user = request.user
+    try:
+
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_from_form = request.POST.get('weekly_pickup')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employee:index'))
+    else:
+        return render(request, 'employee/create.html')
