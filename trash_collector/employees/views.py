97093c119f548f4bc3f3b38de9fd1837a62a9ba2
Tyler Hardiman <Fulldadmode89@gmail.com>
2022-01-26 14:45:28 -0500

    Developed the urls link path, index.html, views, employee base html

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.apps import apps
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from datetime import date
-
 from .models import Employee
+
 # from .models import User
 
 
@@ -14,16 +13,18 @@ from .models import Employee
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
 
-    # Customer = apps.get_model('customer.Customer')
+    
 
-    logged_in_user = request.user
+    logged_in_employee = request.user
     try:
 
-        logged_in_employee = Employee.objects.get(user=logged_in_user)
+        logged_in_employee = Employee.objects.get(user=logged_in_employee)
+
+        
 
         today = date.today()
         
@@ -35,14 +36,6 @@ def index(request):
     except ObjectDoesNotExist:
         return HttpResponseRedirect(reverse('employees:create'))
 
-@login_required
-def details(request, employee_id):
-    logged_in_employee = Employee.objects.get(pk=employee_id)
-    context = {
-        'employee': logged_in_employee
-    }
-    return render(request, 'employees/index.html', context)
-
 
 @login_required
 def create(request):
@@ -51,13 +44,31 @@ def create(request):
         name_from_form = request.POST.get('name')
         address_from_form = request.POST.get('address')
         zip_from_form = request.POST.get('zip_code')
-        weekly_from_form = request.POST.get('weekly_pickup')
-
-        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
-        new_employee.save()
+        logged_in_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
+        logged_in_employee.save()
 
         return HttpResponseRedirect(reverse('employees:index' ))
     else:
         return render(request, 'employees/create.html')
+
+@login_required
+def edit_employee_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employees': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
+
 
 
